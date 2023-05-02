4f1d4a274ee1fd5b03bffd509e2fd1c55d1da99b
konyul <konyulpark@gmail.com>
2023-05-02 07:10:48 +0000

    vv

--- configs/nusc/fgpillarnet/nusc_centerpoint_fppillarnet_h_mlp.py
+++ configs/nusc/fgpillarnet/nusc_centerpoint_fppillarnet_h_mlp.py
@@ -256,5 +256,5 @@ dist_params = dict(backend="nccl", init_method="env://")
 log_level = "INFO"
 work_dir = './work_dirs/{}/'.format(__file__[__file__.rfind('/') + 1:-3])
 load_from = None
-resume_from = 'work_dirs/nusc_centerpoint_fppillarnet_h/epoch_18.pth' 
+resume_from = None
 workflow = [('train', 1)]
